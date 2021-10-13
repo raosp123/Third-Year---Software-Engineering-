ea6281fa20d92d69c58180dc31eddcdcfbcf7b9b
raosp123 <75251451+raosp123@users.noreply.github.com>
2021-10-13 20:10:33 +0100

    Update BinaryTree.java

--- Assignment1/LCA-Java/src/BinaryTree.java
+++ Assignment1/LCA-Java/src/BinaryTree.java
@@ -65,29 +65,4 @@ public class BinaryTree
         // Else return NULL
         return null;
     }
-
-    /* Driver program to test above functions */
-    public static void main(String args[])
-    {
-        BinaryTree tree = new BinaryTree();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        tree.root.right.left = new Node(6);
-        tree.root.right.right = new Node(7);
-
-        Node lca = tree.findLCA(4, 5);
-        if (lca != null)
-            System.out.println("LCA(4, 5) = " + lca.data);
-        else
-            System.out.println("Keys are not present");
-
-        lca = tree.findLCA(4, 10);
-        if (lca != null)
-            System.out.println("LCA(4, 10) = " + lca.data);
-        else
-            System.out.println("Keys are not present");
-    }
 }
